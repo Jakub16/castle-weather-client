0e8e45db2c8de269f7059eecc2f834009b34a7be
kubaz <jakub.zawadzki16@gmail.com>
2022-12-23 19:53:26 +0100

    castle-weather-client module created

---
+++ src/main/java/com/castle/weatherclient/CastleWeatherClientApplication.java
@@ -0,0 +1,13 @@
+package com.castle.weatherclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CastleWeatherClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CastleWeatherClientApplication.class, args);
+    }
+
+}
